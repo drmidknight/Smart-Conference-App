409ef181da358354bed394d4b214854769334489
Catalyst-OTU <bismarkotu1995@gmail.com>
2023-05-31 11:42:32 +0000

    bugs fixed

--- run.py
+++ run.py
@@ -1,8 +1,6 @@
 import uvicorn
 
-# if __name__ == "__main__":
-#     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, workers=4)
 
 if __name__ == '__main__':
-    uvicorn.run("app.main:app", host='localhost', port=8008, log_level="info",workers=4, reload = True)
+    uvicorn.run("app.main:app", host='localhost', port=8000, log_level="info",workers=4, reload = True)
     print("running")
