e0f7b9ab7456bcb979584011198a5d4dd1f8694e
Catalyst-OTU <bismarkotu1995@gmail.com>
2023-05-31 12:50:57 +0000

    bugs fixed

--- app/auth/authentication.py
+++ app/auth/authentication.py
@@ -9,7 +9,7 @@ from pydantic import ValidationError
 from app.utils.config import *
 from jose import JWTError, jwt
 from fastapi.security import (OAuth2PasswordBearer, SecurityScopes)
-from app.routers.admin.schemas import admin
+from app.routers.admin.schemas.admin import *
 from app.routers.admin.endpoint.admin import *
 from typing import Optional
 from app.utils.database import *
