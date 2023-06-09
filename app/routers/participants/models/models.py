a704aeb53b0744c09c361644aef0eeead17dd2db
Catalyst-OTU <bismarkotu1995@gmail.com>
2023-06-09 14:09:27 +0000

    bugs fixed

--- app/routers/participants/models/models.py
+++ app/routers/participants/models/models.py
@@ -25,3 +25,25 @@ class Participant(Base):
     updated_at = db.Column(TIMESTAMP, nullable=False,server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
     #event = relationship("Event", back_populates="participants")
     #attendance_id = db.Column(db.Integer, ForeignKey('attendances.id'))
+
+
+
+
+
+class Participant(Base):
+    __tablename__ = 'add_participant_fields'
+    id = db.Column(db.Integer, primary_key=True)
+    field_name = db.Column(db.String(255), nullable=True)
+    phone_number = db.Column(db.String(255), nullable=True, unique=True)
+    gender = db.Column(db.String(255), nullable=True)
+    email = db.Column(db.String(255), nullable=True, unique=True)
+    organization = db.Column(db.String(255), nullable=True)
+    status = db.Column(Boolean, default=False, index=False)
+    attend_by = db.Column(db.String(255), nullable=True)
+    registration_time = db.Column(db.String(255), nullable=True)
+    location = db.Column(db.String(255), nullable=True)
+    event_id = db.Column(db.Integer, ForeignKey('events.id'))
+    created_at = db.Column(TIMESTAMP, nullable=False,server_default=text("CURRENT_TIMESTAMP"))
+    updated_at = db.Column(TIMESTAMP, nullable=False,server_default=text("CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
+    #event = relationship("Event", back_populates="participants")
+    #attendance_id = db.Column(db.Integer, ForeignKey('attendances.id'))
